5310e904b34f1ddfa2a99bfe340a83a34a49fe74
Faizan-bff091 <faizanaliirfan99@gmail.com>
2025-04-30 16:26:47 +0100

    Add files via upload

---
+++ script1.lua
@@ -0,0 +1,2 @@
+Join my discord server as this script is being worked on and i will have sneek peeks their 
+discord server link: https://discord.gg/jCvZZwy3Vz
---
+++ script2.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xycon-developer/Xycon-Lucky-battlegrounds-script/main/Script"))()
+
+Join my discord server for more scripts 
+Server link: https://discord.gg/jCvZZwy3Vz

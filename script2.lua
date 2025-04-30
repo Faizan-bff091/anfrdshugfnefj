6b705c9bc4a93622bf38691b429df4bbd77fcb01
Faizan-bff091 <faizanaliirfan99@gmail.com>
2025-04-30 16:35:46 +0100

    Delete script2.lua

--- script2.lua
+++
@@ -1,4 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Xycon-developer/Xycon-Lucky-battlegrounds-script/main/Script"))()
-
-Join my discord server for more scripts 
-Server link: https://discord.gg/jCvZZwy3Vz
